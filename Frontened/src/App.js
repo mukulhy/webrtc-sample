07b295349ca59e6c98868e022e7a44ce807bcd66
mukulhy <yadavh3034@gmail.com>
2024-06-06 21:09:13 +0530

    commit initial frontened

---
+++ Frontened/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import VideoApp from './components/videoApp';
+import MultipleVideoApp from './components/multipleVideoApp';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <VideoApp /> */}
+      <MultipleVideoApp />
+    </div>
+  );
+}
+
+export default App;
